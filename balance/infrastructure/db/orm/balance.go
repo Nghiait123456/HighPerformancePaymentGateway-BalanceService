fe9ab38327a60462d19818ebb5fac5cbdb1a7810
nghiapm <nghiapm@appota.com>
2022-08-12 08:47:05 +0700

    update orm balance

--- balance/infrastructure/db/orm/balance.go
+++ balance/infrastructure/db/orm/balance.go
@@ -4,7 +4,15 @@ type Balance struct {
 	ID          uint32 `gorm:"primaryKey;<-:create"`
 	Balance     uint32
 	PartnerCode string
-	status      string
+	Status      string
 	CreatedAt   uint32
 	UpdatedAt   uint32
+}
+
+const (
+	STATUS_ACTIVE = "active"
+)
+
+func (b *Balance) IsActive() bool {
+	return b.Status == STATUS_ACTIVE
 }
