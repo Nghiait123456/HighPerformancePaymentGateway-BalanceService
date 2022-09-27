9f0fe888368111b3e7944374f774c9a6f2dc440d
nghiapm <nghiapm@appota.com>
2022-09-27 15:57:38 +0700

    test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func handle(c *fiber.Ctx) error {
-	panic("have error2222   111sssss222dddddccc                v                                        ")
+	panic("have error2222   111sssss222dddddccc                v                                           ")
 	return fiber.NewError(500, "have erorssss")
 	return c.SendString("Hello, World!")
 
